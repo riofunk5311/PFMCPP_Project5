f25f00bf834ba58c7ede0259ba3d2e466d6173a8
Ryota Sugawara <40291136+riofunk5311@users.noreply.github.com>
2022-04-15 15:19:32 +0000

    Initial Commmit

--- main.cpp
+++ main.cpp
@@ -70,23 +70,396 @@ write 3 UDTs below that EACH have:
  You will use those in Part 3 of this project.
 
  */
+#include<iostream>
 
 /*
  copied UDT 1:
  */
 
+struct CustomBuildPc
+{
+    int numOfCpus = 3;
+    int amountOfRamInGb = 32;
+    int amountOfHddInGb;
+    int numberOfOs = 1;
+    int amountOfWattage;
+    int photo = 50;
+
+    CustomBuildPc();
+    ~CustomBuildPc();
+
+    struct Cpu
+    {
+        int numCpuCore = 2;
+        std::string manufacturer = "Intel";
+        std::string model = "Xeon";
+        std::string program = "Photoshop";
+
+        Cpu();
+        ~Cpu();
+
+        int numPhysicalCpus;
+        float clockSpeedInGhz;
+
+        void runProgram();
+        float powerConsume(float timeCpuRunningInMinutes, int cpuUsage, bool multiThreadingOn = true);
+        float getTimeToCompile(float sizeOfFile, std::string compileError);
+        float clockUp(float maxClockGain);
+    };
+
+    void writeProgram(std::string language);
+    void createVideo(Cpu& cpuB, std::string nameOfCpu);
+    void editPhoto();
+    void photoPrint(int photoYouWantToPrint);
+};
+
+CustomBuildPc::CustomBuildPc() : 
+amountOfHddInGb(2000), 
+amountOfWattage(600)
+{
+    std::cout << "CustomBuildPc being constructed!" << std::endl;
+}
+
+CustomBuildPc::~CustomBuildPc()
+{
+    std::cout << "CustomBuildPc being destructed!" << std::endl;
+}
+
+CustomBuildPc::Cpu::Cpu() : 
+numPhysicalCpus(4),
+clockSpeedInGhz(2.8f)
+{
+    std::cout << "Cpu being constructed!" << std::endl;
+}
+
+CustomBuildPc::Cpu::~Cpu()
+{
+    std::cout << "Cpu being destructed!" << std::endl;
+}
+
+void CustomBuildPc::Cpu::runProgram()
+{
+    std::cout << "Cpu is running at " << clockSpeedInGhz << " Hz" << " on " << numPhysicalCpus << " CPUs" <<  std::endl;
+    std::cout << "CustonBuildPc::Cpu::runProgram() " << program << std::endl;
+}
+
+float CustomBuildPc::Cpu::powerConsume(float timeCpuRunningInMinutes, int cpuUsage, bool multiThreadingOn)
+{
+    if ( multiThreadingOn == true && cpuUsage == 5 )
+    {
+        std::cout << "Power Consumption is high" << std::endl;
+    }
+
+    return clockSpeedInGhz * timeCpuRunningInMinutes;
+}
+
+float CustomBuildPc::Cpu::getTimeToCompile(float sizeOfFile, std::string compileError)
+{
+    if ( sizeOfFile > 100.0f )
+    {
+        std::cout << compileError << std::endl;
+    }
+    
+    return sizeOfFile;
+}
+
+void CustomBuildPc::writeProgram(std::string language)
+{
+    std::cout << language << std::endl;
+}
+
+void CustomBuildPc::createVideo(Cpu &cpuB, std::string nameOfCpu)
+{
+     std::cout << cpuB.numCpuCore << "\n" << nameOfCpu << std::endl;
+}
+
+void CustomBuildPc::editPhoto()
+{
+    std::cout << "Amount of wattage: " << amountOfWattage << std::endl;
+    std::cout << "CustomBuildPc::editPhoto() " << photo << std::endl;
+}
+
+float CustomBuildPc::Cpu::clockUp(float maxClockGain)
+{
+    std::cout << "Base Clock is " << clockSpeedInGhz << std::endl;
+    float clockIncrement = 0.1f;
+    float newBaseClock;
+
+    if ( maxClockGain >= 1.0f )
+    {
+        for ( float i = 0.1f; i <= maxClockGain; i += clockIncrement )
+        {
+            newBaseClock = i + clockSpeedInGhz;
+            std::cout << "Running at " << newBaseClock << " Ghz " << std::endl;
+        }
+    }
+    else 
+    {
+        std::cout << "Cpu is not running properly" << std::endl;
+    }
+
+    return 0;
+}        
+
+void CustomBuildPc::photoPrint(int photoYouWantToPrint)
+{
+    int i = 1;
+    if ( photoYouWantToPrint <= photo)
+    {
+        while ( i < photoYouWantToPrint )
+        {
+            std::cout << "Printed " << i << " photo" << std::endl;
+            std::cout << (photo - 1) << " left" << std::endl;
+            ++i;
+            --photo;
+        }
+        std::cout << "Totally " << photoYouWantToPrint << " are printed" << std::endl;
+    }
+    return;
+}
+
 /*
  copied UDT 2:
  */
+
+struct PhotoShoot
+{
+    int numCameras = 4;
+    int numHairMakeUpArtists;
+    int numModel = 8;
+    std::string nameOfCompanies = "Apple";
+
+    PhotoShoot();
+    ~PhotoShoot();
+
+    int amountOfMemoryCard;
+    float amountOfClothes;
+
+    struct Camera
+    {
+        double shutterSpeed { 0.000125 }; 
+        bool isMirrorLess = true;
+        std::string brand = "Nikon";
+        std::string model;
+        std::string typeOfLens;
+
+        Camera();
+        ~Camera();
+
+        void shootVideo();
+        void changeLens();
+        int takePhoto();
+        void countModel(int personTaken);
+    };
+
+    void makePr();
+    void makeNewFashionStyle(Camera& cameraA);
+    int bookStudio(int daysToShoot, int numOfCrews);
+    int callHairMakeArtist(int personToBeShot);
+};
+
+PhotoShoot::PhotoShoot() : 
+amountOfMemoryCard(512),
+amountOfClothes(25.0f)
+{
+    std::cout << "PhotoShoot being constructed!" << std::endl;
+}
+
+PhotoShoot::~PhotoShoot()
+{
+    std::cout << "PhotoShoot being destructed!" << std::endl;    
+}
+
+PhotoShoot::Camera::Camera() : 
+model("Z9"),
+typeOfLens("Standard")
+{
+    std::cout << "Camera being constructed!" << std::endl;
+}
+
+PhotoShoot::Camera::~Camera()
+{
+    std::cout << "Camera being destructed!" << std::endl;    
+}
+
+void PhotoShoot::makePr()
+{
+    std::cout << "PhotoShoot::makePr() " << nameOfCompanies << std::endl;
+}
+
+void PhotoShoot::makeNewFashionStyle(Camera &cameraA)
+{
+    std::cout << "We need " << amountOfMemoryCard << " of Memory Card and " << amountOfClothes << " of clothes" << std::endl;
+    std::cout << cameraA.brand << std::endl;
+}
+
+int PhotoShoot::bookStudio(int daysToShoot, int numOfCrews)
+{
+    std::cout << "We need " << daysToShoot << " studio days and " << numOfCrews << " Crews" << std::endl;
+    return 0;
+}
+
+void PhotoShoot::Camera::shootVideo()
+{
+    if ( isMirrorLess == false)
+    {
+        std::string anotherLens = "parfoca";
+    }
+
+    std::cout << "We use " << model << std::endl;
+}
+
+void PhotoShoot::Camera::changeLens()
+{
+    std::cout << "PhotoShoot::Camera::changeLens() "<< typeOfLens << std::endl;
+}
+
+int PhotoShoot::Camera::takePhoto()
+{
+    if ( isMirrorLess )
+    {
+       std::cout << brand << std::endl;
+    }
+    int photoTaken = 2;
+    return photoTaken;
+}
+
+void PhotoShoot::Camera::countModel(int personTaken)
+{
+    PhotoShoot photoShoot;
+    
+    for ( int i = 1; i < personTaken; ++i )
+    {
+        std::cout << i << " Person is done" << std::endl;
+        std::cout << photoShoot.numModel - 1 << " models left" << std::endl;
+        photoShoot.numModel -= 1;
+    }
+    return;
+}
+
+int PhotoShoot::callHairMakeArtist(int personToBeShot)
+{
+    int i = 1;
+    numHairMakeUpArtists = 1;
+
+    while( i < personToBeShot )
+    {
+        std::cout << i << " model here" << std::endl;
+        std::cout << "we need " << numHairMakeUpArtists << " hair make up artists" << std::endl;
+
+        ++i;
+        ++numHairMakeUpArtists;
+    }
+    
+    return numHairMakeUpArtists;
+}
 
 /*
  copied UDT 3:
  */
 
+struct Keyboard
+{
+    Keyboard();
+    ~Keyboard();
+
+    int numUsbPorts;
+    int characterTyped;
+    int numKeysTyped;
+    float distanceKeyTravel;
+    std::string keyLanguage;
+    float bluetoothChannel;
+   
+    int typeCharacters();
+    void connectToUsb(int numPortsConnected);
+    void sendKeyCommands(int keyInput, std::string keyLayout);
+    int deleteCharacter(int pressDeleteKey);
+};
+
+Keyboard::Keyboard() : 
+numUsbPorts(2),
+characterTyped(40),
+numKeysTyped(20),
+distanceKeyTravel(15.0f),
+keyLanguage("US"),
+bluetoothChannel(2.0f)
+{
+    std::cout << "Keyboard being constructed!" << std::endl;
+}
+
+Keyboard::~Keyboard()
+{
+    std::cout << "Keyboard being destructed!" << std::endl;
+}
+
+int Keyboard::typeCharacters()
+{
+    std::cout << "Keyboard::typeCharacters() " << characterTyped << std::endl;
+    return characterTyped;
+}
+
+void Keyboard::connectToUsb(int numPortsConnected)
+{
+    if ( numPortsConnected == 2 )
+    {
+        std::cout << "All USB Ports Are Connected" << std::endl;
+    }
+
+    std::cout << numUsbPorts << " usb ports are being used" << std::endl;
+}
+
+void Keyboard::sendKeyCommands(int keyInput, std::string keyLayout)
+{
+    if ( keyInput == 109 )
+    {
+        std::cout << "It is Apple Keyboard " << keyLayout << std::endl;
+    }
+}
+
+int Keyboard::deleteCharacter(int pressDeleteKey)
+{
+    if ( pressDeleteKey >= characterTyped )
+    {
+        std::cout << "You deleted all words" << std::endl;
+    }
+    else
+    {
+        for ( int i = 1; i <= pressDeleteKey; ++i )
+        {
+            std::cout << "You deleted " << i << " character(s)" << std::endl;
+            characterTyped -= 1;
+        }
+
+        std::cout << "You deleted all words" << std::endl;
+    }
+    return characterTyped;
+}
+
 /*
  new UDT 4:
  with 2 member functions
  */
+
+struct OperatingSystem
+{
+    CustomBuildPc customBuildPc;
+    CustomBuildPc::Cpu cpu;
+
+    OperatingSystem();
+    ~OperatingSystem();
+
+    // start from here
+};
+
+OperatingSystem::OperatingSystem()
+{
+    std::cout << "OperatingSystem is being constructed!" << std::endl;
+}
+
+OperatingSystem::~OperatingSystem()
+{
+    std::cout << "OperatingSystem is being destructed!" << std::endl;
+}
 
 /*
  new UDT 5:
